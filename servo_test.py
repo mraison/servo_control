b4ad2a4faf50930f73f9e70e0b6c493233f1b682
Matthew Raison <mraison@wayfair.com>
2020-07-26 23:14:35 -0400

    commenting out some odd junk I don't really need.

--- servo_test.py
+++ servo_test.py
@@ -26,53 +26,56 @@ class ServoClient(object):
 		startingDutyCycle = self._convertDegreesToServo(startingAnge)
 		
 		# self._convertDegreesToServo(self._startingAnge)
-		self.setDutyCycle(startingDutyCycle, 0.25)
+		self._servo.ChangeDutyCycle(
+			startingDutyCycle
+		)
+		# self.setDutyCycle(startingDutyCycle)
 		#self.moveToAngle(self._startingAnge)
 		
-	def setDutyCycle(self, newDutyCycle=0, stepSize=0):
-		if newDutyCycle > self._max_duty_cycle or newDutyCycle < self._min_duty_cycle:
-			return 1
-		
-		if newDutyCycle == self._startingDutyCycle:
-			return 0
-		
-		current_duty_cycle = self._startingDutyCycle
-		self._startingDutyCycle = newDutyCycle
-		
-		# while current_duty_cycle < newDutyCycle:
-		max_r = max(current_duty_cycle, newDutyCycle)
-		min_r = min(current_duty_cycle, newDutyCycle)
-		do_flip = current_duty_cycle > newDutyCycle
-		r = numpy.arange(min_r, max_r, stepSize)
-		if do_flip:
-			r = numpy.flip(r)
-		
-		for i in r:
-			self._servo.ChangeDutyCycle(
-				i
-			)
-			time.sleep(0.5)
-		
-		self._servo.ChangeDutyCycle(0)
+	# def setDutyCycle(self, newDutyCycle=0, stepSize=0):
+	# 	if newDutyCycle > self._max_duty_cycle or newDutyCycle < self._min_duty_cycle:
+	# 		return 1
+	#
+	# 	if newDutyCycle == self._startingDutyCycle:
+	# 		return 0
+	#
+	# 	current_duty_cycle = self._startingDutyCycle
+	# 	self._startingDutyCycle = newDutyCycle
+	#
+	# 	# while current_duty_cycle < newDutyCycle:
+	# 	max_r = max(current_duty_cycle, newDutyCycle)
+	# 	min_r = min(current_duty_cycle, newDutyCycle)
+	# 	do_flip = current_duty_cycle > newDutyCycle
+	# 	r = numpy.arange(min_r, max_r, stepSize)
+	# 	if do_flip:
+	# 		r = numpy.flip(r)
+	#
+	# 	for i in r:
+	# 		self._servo.ChangeDutyCycle(
+	# 			i
+	# 		)
+	# 		time.sleep(0.5)
+	#
+	# 	self._servo.ChangeDutyCycle(0)
 
-	# return 0 if everything is fine. if it's anything else...it's an error.
-	def moveToAngle(self, degrees):
-		if degrees > 180 or degrees < 0:
-			return 1
-		
-		if degrees == self._startingAnge:
-			return 0
-		
-		self._startingAnge = degrees
-		#range for servo is from 2 to 12. 7 is 90, 12 is 180, 2 is 0.
-		# normalization is servo num = degrees/18 + 2
-		# This is the angle writes.
-		self._servo.ChangeDutyCycle(
-			self._convertDegreesToServo(degrees)
-		)
-		time.sleep(0.5)
-		self._servo.ChangeDutyCycle(0)
-		time.sleep(0.5)
+	## return 0 if everything is fine. if it's anything else...it's an error.
+	# def moveToAngle(self, degrees):
+	# 	if degrees > 180 or degrees < 0:
+	# 		return 1
+	#
+	# 	if degrees == self._startingAnge:
+	# 		return 0
+	#
+	# 	self._startingAnge = degrees
+	# 	#range for servo is from 2 to 12. 7 is 90, 12 is 180, 2 is 0.
+	# 	# normalization is servo num = degrees/18 + 2
+	# 	# This is the angle writes.
+	# 	self._servo.ChangeDutyCycle(
+	# 		self._convertDegreesToServo(degrees)
+	# 	)
+	# 	time.sleep(0.5)
+	# 	self._servo.ChangeDutyCycle(0)
+	# 	time.sleep(0.5)
 
 	def _convertDegreesToServo(self, degrees):
 		return int(round(((degrees / 18) + 2)))
